297553f7d98f6f72211760fec3275f2326c8b791
gabriel cardoso da lapa <98766972+biellapa@users.noreply.github.com>
2023-01-24 13:37:38 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Countdown from "./countdown.js";
 
-const tempoParaOWWB = new Countdown("18 january 2024 20:00:00 GMT-0300");
+const tempoParaOWWB = new Countdown("18 january 2023 20:00:00 GMT-0300");
 const tempos = document.querySelectorAll("[data-time]");
 
 function mostrarTempo() {
